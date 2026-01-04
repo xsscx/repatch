5826b140e51c72424aa012a4ea083c2ca9045084
David Hoyt <xss@xss.cx>
2026-01-04 17:44:22 -0500

    Fix: Tool path in RunTests.sh (#430)
    Signed-off-by: D Hoyt xss@xss.cx

--- Testing/Overprint/RunTests.sh
+++ Testing/Overprint/RunTests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #################################################################################
-# Overprint/RunTests.sh | iccMAX Project
-# Copyright (C) 2024-2025 The International Color Consortium. 
+# Overprint/RunTests.sh | iccDEV Project
+# Copyright (C) 2024-2026 The International Color Consortium. 
 #                                        All rights reserved.
 # 
 #
-#  Last Updated: Mon Mar 24 16:40:19 EDT 2025 by David Hoyt
-#  date -d @1742848819
-#  Mon Mar 24 16:40:19 EDT 2025
+#  Last Updated: Sun Jan  4 10:03:49 PM UTC 2026 by David Hoyt
+#                Fix PATH Issue
+#                PATH error was masked by CI hardcoding
 #
 #
 #
@@ -22,7 +22,7 @@
 echo "====================== Entering Overprint/RunTests.sh =========================="
 
 # Properly handle newline-separated paths as a list
-find ../Build/Tools -type f -perm -111 -exec dirname {} \; | sort -u | while read -r d; do
+find ../../Build/Tools -type f -perm -111 -exec dirname {} \; | sort -u | while read -r d; do
   abs_path=$(cd "$d" && pwd)
   PATH="$abs_path:$PATH"
 done
